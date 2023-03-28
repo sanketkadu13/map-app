745c9bf95cb214ad41bac21bfc1e285852c4a290
sanketkadu13 <119657135+sanketkadu13@users.noreply.github.com>
2023-03-28 10:56:46 +0530

    Initial commit

---
+++ src/Map.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+
+class Map extends Component {
+  state = {
+    currentLocation: { lat: null, lng: null }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      this.setState({
+        currentLocation: { lat: latitude, lng: longitude }
+      });
+    });
+  }
+
+  render() {
+    return (
+      <MapContainer center={this.state.currentLocation} zoom={13} style={{ height: "100vh" }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={this.state.currentLocation}>
+          <Popup>
+            Your current location
+          </Popup>
+        </Marker>
+      </MapContainer>
+    );
+  }
+}
+
+export default Map;
